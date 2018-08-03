146664c63056751d46858f1c8dbf30296abc0021
BuddhaL1T3 <jmriggs75@gmail.com>
2018-08-03 15:02:09 -0400

    updated cors

--- server.js
+++ server.js
@@ -15,14 +15,22 @@ const dogsRouter = require('./routes/dogs.js');
 const app = express();
 
 // Log all requests. Skip logging during
-app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
-  skip: (req, res) => process.env.NODE_ENV === 'test'
-}));
+app.use(
+  morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
+);
 
-// Cross origin
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}));
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 // API endpoints
 app.use('/api/cats', catsRouter);
